9973151fe03d60684b7e5882a7d8a92e2fe5bd8c
Muse <mse450@hotmail.com>
2019-02-28 14:14:43 +0100

    Galaga filer tilføjet projekt og game class herunder

---
+++ SU19-exercises/Galaga-Exercise-1/Game.cs
@@ -0,0 +1,48 @@
+using System;
+using DIKUArcade;
+using DIKUArcade.EventBus;
+using DIKUArcade.Timers;
+
+namespace Galaga_Exercise_1 {
+    public class Game  : IGameEventProcessor<object> {
+        private Window win;
+        private DIKUArcade.Timers.GameTimer gameTimer;
+        public Game() {
+// TODO: Choose some reasonable values for the window and timer constructor.
+// For the window, we recommend a 500x500 resolution (a 1:1 aspect ratio).
+            win = new Window("Window-name",500, 500);
+            gameTimer = new GameTimer(60,60);
+        }
+        public void GameLoop() {
+            
+            while(win.IsRunning()) {
+                gameTimer.MeasureTime();
+                while (gameTimer.ShouldUpdate()) {
+                    win.PollEvents();
+        // Update game logic here
+                }
+                if (gameTimer.ShouldRender()) {
+                    win.Clear();
+        // Render gameplay entities here
+                    win.SwapBuffers();
+                }
+                if (gameTimer.ShouldReset()) {
+        // 1 second has passed - display last captured ups and fps
+                    win.Title = "Galaga | UPS: " + gameTimer.CapturedUpdates +
+                                ", FPS: " + gameTimer.CapturedFrames;
+                }
+            }
+        }
+        public void KeyPress(string key) {
+            throw new NotImplementedException();
+        }
+        public void KeyRelease(string key) {
+            throw new NotImplementedException();
+        }
+        public void ProcessEvent(GameEventType eventType,
+            GameEvent<object> gameEvent) {
+            throw new NotImplementedException();
+        }
+    }
+        
+    }
--- SU19-exercises/Galaga-Exercise-1/Program.cs
+++ SU19-exercises/Galaga-Exercise-1/Program.cs
@@ -1,5 +1,8 @@
 ﻿namespace Galaga_Exercise_1 {
     internal class Program {
-        public static void Main(string[] args) { }
+        public static void Main(string[] args) {
+            Game firstgame = new Game();
+            firstgame.GameLoop();
+        }
     }
 }
