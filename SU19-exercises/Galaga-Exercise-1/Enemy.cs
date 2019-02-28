17ddb95f18e9f09ea024e7b8b4014b5c3b1a1010
Mikael H <mikael.holsve@gmail.com>
2019-02-28 18:36:56 +0100

    Added enemies that work sort of

--- SU19-exercises/Galaga-Exercise-1/Enemy.cs
+++ SU19-exercises/Galaga-Exercise-1/Enemy.cs
@@ -9,46 +9,8 @@ namespace Galaga_Exercise_1 {
     public class Enemy : Entity {
         private Game game;
         
-        public List<Image> enemyStrides;
-
-        public List<Enemy> enemies;
-        public Enemy(Game game, Shape shape, IBaseImage image) : base(shape, image) {
+        public Enemy(Game game, Shape shape, ImageStride image) : base(shape, image) {
         
-        enemyStrides = ImageStride.CreateStrides(4,
-        Path.Combine("Assets", "Images", "BlueMonster.png"));
-        enemies = new List<Enemy>();
-    }
-
-        ImageStride strides = new ImageStride(int 80, enemyStrides);
-
-        public void AddEnemies() {
-            Enemy Enemy1 = new Enemy(game, new DynamicShape(new Vec2F(0.20f, 0.9f), 
-                new Vec2F(0.1f, 0.1f)),);
-            Enemy Enemy2 = new Enemy(game, new DynamicShape(new Vec2F(0.30f, 0.9f), 
-                new Vec2F(0.1f, 0.1f)),);
-            Enemy Enemy3 = new Enemy(game, new DynamicShape(new Vec2F(0.40f, 0.9f), 
-                new Vec2F(0.1f, 0.1f)),);
-            Enemy Enemy4 = new Enemy(game, new DynamicShape(new Vec2F(0.50f, 0.9f), 
-                new Vec2F(0.1f, 0.1f)),);
-            Enemy Enemy5 = new Enemy(game, new DynamicShape(new Vec2F(0.60f, 0.9f), 
-                new Vec2F(0.1f, 0.1f)),);
-            Enemy Enemy6 = new Enemy(game, new DynamicShape(new Vec2F(0.70f, 0.9f), 
-                new Vec2F(0.1f, 0.1f)),);
-            Enemy Enemy7 = new Enemy(game, new DynamicShape(new Vec2F(0.80f, 0.9f), 
-                new Vec2F(0.1f, 0.1f)),);
-            Enemy Enemy8 = new Enemy(game, new DynamicShape(new Vec2F(0.90f, 0.9f), 
-                new Vec2F(0.1f, 0.1f)),);
-            
-            enemies.Add(Enemy1);
-            enemies.Add(Enemy2);
-            enemies.Add(Enemy3);
-            enemies.Add(Enemy4);
-            enemies.Add(Enemy5);
-            enemies.Add(Enemy6);
-            enemies.Add(Enemy7);
-            enemies.Add(Enemy8);
-            
         }
-        
     }
 }
