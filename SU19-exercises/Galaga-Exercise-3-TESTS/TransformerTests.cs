971ad4e48446ba3ff03286661928974872bdfdcd
Frederik Kragh <kragh96@hotmail.com>
2019-03-21 18:35:55 +0100

    created TransformerTests class

---
+++ SU19-exercises/Galaga-Exercise-3-TESTS/TransformerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+using Galaga_Exercise_1.GameStateType
+
+
+namespace Galaga_Exercise_3_TESTS {
+    [TestFixture]
+    public class TransformerTests {
+        [Test]
+        public void TransformStringToStateTest("GAME_RUNNING") {
+            Assert.Equals(GameStateType.GameRunning);
+        }
+        
+        [Test]
+        public void TransformStateToStringTest() {
+            Assert.True(true);
+        }
+    }
+}
