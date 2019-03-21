022af65913175b8e3c8faa017597a437d4450482
Muse <mse450@hotmail.com>
2019-03-21 18:29:13 +0100

    Gamestatetype og statetransformer

---
+++ SU19-exercises/Galaga-Exercise-3/GameStateType/GameStateType.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace Galaga_Exercise_1.GameStateType {
+    public enum GameStateType {
+        GameRunning,
+        GamePaused,
+        MainMenu
+    }
+    public class StateTransformer {
+
+        public static GameStateType TransformStringToState(string state) {
+
+            switch (state) {
+                case "GAME_RUNNING":
+                    return GameStateType.GameRunning;
+                    break;
+                case "GAME_PAUSED":
+                    return GameStateType.GamePaused;
+                    break;
+                case "GAME_MAINMENU":
+                    return GameStateType.MainMenu;
+                    break;
+                default:
+                    throw new ArgumentException("ERROR");
+                    break;  
+            } 
+        }
+
+        public static string TransformStateToString(GameStateType state) {
+            switch (state) {
+            case GameStateType.GameRunning:
+                return "GAME_RUNNING";
+                break;
+
+            case GameStateType.GamePaused:
+                return "GAME_PAUSED";
+                break;
+            case GameStateType.MainMenu:
+                return "GAME_MAINMENU";
+                break;
+            default:
+                throw new ArgumentException("ERROR");
+                break;
+            }        
+        }
+        
+        
+    }
+}
