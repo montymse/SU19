0793ece14e51d83378f69a52360d3a102fa9ad8e
Frederik Kragh <kragh96@hotmail.com>
2019-03-21 18:51:33 +0100

    created tests for StateTransformers

--- SU19-exercises/Galaga-Exercise-3-TESTS/TransformerTests.cs
+++ SU19-exercises/Galaga-Exercise-3-TESTS/TransformerTests.cs
@@ -1,19 +1,40 @@
 ﻿using System;
 using NUnit.Framework;
-using Galaga_Exercise_1.GameStateType
+using Galaga_Exercise_1.GameStateType;
 
 
 namespace Galaga_Exercise_3_TESTS {
     [TestFixture]
     public class TransformerTests {
+        
         [Test]
-        public void TransformStringToStateTest("GAME_RUNNING") {
-            Assert.Equals(GameStateType.GameRunning);
+        public void TransformStringToStateGameRunningTest() {
+            Assert.AreEqual(StateTransformer.TransformStringToState("GAME_RUNNING"), GameStateType.GameRunning);
         }
         
         [Test]
-        public void TransformStateToStringTest() {
-            Assert.True(true);
+        public void TransformStringToStateGamePausedTest() {
+            Assert.AreEqual(StateTransformer.TransformStringToState("GAME_PAUSED"), GameStateType.GamePaused);
+        }
+        
+        [Test]
+        public void TransformStringToStateMainMenuTest() {
+            Assert.AreEqual(StateTransformer.TransformStringToState("GAME_MAINMENU"), GameStateType.MainMenu);
+        }
+        
+        [Test]
+        public void TransformStateToStringGameRunningTest() {
+            Assert.AreEqual(GameStateType.GameRunning, StateTransformer.TransformStringToState("GAME_RUNNING"));
+        }
+        
+        [Test]
+        public void TransformStateToStringGamePausedTest() {
+            Assert.AreEqual(GameStateType.GamePaused, StateTransformer.TransformStringToState("GAME_PAUSED"));
+        }
+        
+        [Test]
+        public void TransformStateToStringMainMenuTest() {
+            Assert.AreEqual(GameStateType.MainMenu, StateTransformer.TransformStringToState("GAME_MAINMENU"));
         }
     }
 }
