521fabd490c6359cbc65780a47609e8bcfc09200
Mikael H <mikael.holsve@gmail.com>
2019-03-21 18:08:28 +0100

    Added GameEventBus and test project

---
+++ SU19-exercises/Galaga-Exercise-3-TESTS/Tests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Galaga_Exercise_3_TESTS {
+    [TestFixture]
+    public class Tests {
+        [Test]
+        public void Test1() {
+            Assert.True(true);
+        }
+    }
+}
