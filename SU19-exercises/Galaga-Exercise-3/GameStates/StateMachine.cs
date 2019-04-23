bf3863bd55698f645249e4ced8f724d5aec5a335
Mikael H <mikael.holsve@gmail.com>
2019-04-23 21:20:30 +0200

    Implemented ProcessEvent() (sort of)

--- SU19-exercises/Galaga-Exercise-3/GameStates/StateMachine.cs
+++ SU19-exercises/Galaga-Exercise-3/GameStates/StateMachine.cs
@@ -16,22 +16,46 @@ namespace GalagaGame.GalagaState {
 
         private void SwitchState(StateTransformer.GameStateType stateType) {
             switch (stateType) {
-            case StateTransformer.GameStateType.GameRunning:
-                ActiveState = GameRunning.GetInstance();
-                break;
-            case StateTransformer.GameStateType.GamePaused:
-                ActiveState = GamePaused.GetInstance();
-                break;
-            case StateTransformer.GameStateType.MainMenu:
-                ActiveState = MainMenu.GetInstance();
-                break;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(stateType), stateType, null);
+                case StateTransformer.GameStateType.GameRunning:
+                    ActiveState = GameRunning.GetInstance();
+                    break;
+                case StateTransformer.GameStateType.GamePaused:
+                    ActiveState = GamePaused.GetInstance();
+                    break;
+                case StateTransformer.GameStateType.MainMenu:
+                    ActiveState = MainMenu.GetInstance();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(stateType), stateType, null);
             }
         }
 
         public void ProcessEvent(GameEventType eventType, GameEvent<object> gameEvent) {
-            throw new System.NotImplementedException();
+            /*
+             *
+             * I have added the ability to change states. This implementation is based on the
+             * little code snippet in p. 8 of the assignment text.
+             *
+             * -Mikael
+             * 
+             */
+            if (eventType == GameEventType.GameStateEvent) {
+                if (gameEvent.Message == "CHANGE_STATE") {
+                    switch (gameEvent.Parameter1) {
+                        case "GAME_RUNNING":
+                            SwitchState(StateTransformer.GameStateType.GameRunning);
+                            break;
+                        case "GAME_MAINMENU":
+                            SwitchState(StateTransformer.GameStateType.MainMenu);
+                            break;
+                        case "GAME_PAUSED":
+                            SwitchState(StateTransformer.GameStateType.GamePaused);
+                            break;
+                        default:
+                            throw new ArgumentException("Invalid parameter");
+                    }
+                }
+            }
         }
     }
 }
