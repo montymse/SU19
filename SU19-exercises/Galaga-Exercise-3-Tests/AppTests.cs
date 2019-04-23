8220685fd25a507d4e1e7ccd050b392d1ec93dfb
Mikael H <mikael.holsve@gmail.com>
2019-04-23 20:25:58 +0200

    Added test project

---
+++ SU19-exercises/Galaga-Exercise-3-Tests/AppTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Galaga_Exercise_3_Tests {
+    [TestFixture]
+    public class Tests {
+        [SetUp]
+        public void BeforeEachTest() {
+        }
+
+        [Test]
+        public void Test() {
+            Assert.IsTrue(true);
+        }
+    }
+}
