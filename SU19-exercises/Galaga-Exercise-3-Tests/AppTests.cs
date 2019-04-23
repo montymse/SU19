62360a7cf35a53694267260f7e72fb802f961eb3
Mikael H <mikael.holsve@gmail.com>
2019-04-23 20:45:30 +0200

    Added tests for TransformStateToString and TransformStringToState

--- SU19-exercises/Galaga-Exercise-3-Tests/AppTests.cs
+++ SU19-exercises/Galaga-Exercise-3-Tests/AppTests.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Linq;
 using NUnit.Framework;
+using Galaga_Exercise_3;
+using Galaga_Exercise_3.GameStateType;
 
 namespace Galaga_Exercise_3_Tests {
     [TestFixture]
@@ -11,8 +13,17 @@ namespace Galaga_Exercise_3_Tests {
         }
 
         [Test]
-        public void Test() {
-            Assert.IsTrue(true);
+        public void TransformStateToString() {
+            Assert.AreEqual(StateTransformer.TransformStateToString(StateTransformer.GameStateType.MainMenu), "GAME_MAINMENU");
+            Assert.AreEqual(StateTransformer.TransformStateToString(StateTransformer.GameStateType.GamePaused), "GAME_PAUSED");
+            Assert.AreEqual(StateTransformer.TransformStateToString(StateTransformer.GameStateType.GameRunning), "GAME_RUNNING");
+        }
+        
+        [Test]
+        public void TransformStringToState() {
+            Assert.AreEqual(StateTransformer.TransformStringToState("GAME_MAINMENU"),StateTransformer.GameStateType.MainMenu);
+            Assert.AreEqual(StateTransformer.TransformStringToState("GAME_PAUSED"),StateTransformer.GameStateType.GamePaused);
+            Assert.AreEqual(StateTransformer.TransformStringToState("GAME_RUNNING"),StateTransformer.GameStateType.GameRunning);
         }
     }
 }
