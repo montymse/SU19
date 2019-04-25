ea65d8e694d2dd7367afdb400b7a5ba080f8b8c4
Mikael H <mikael.holsve@gmail.com>
2019-04-25 19:27:38 +0200

    Nuked some shit. Added more shit

---
+++ SU19-exercises/SpaceTaxi-1/Opener.cs
@@ -0,0 +1,18 @@
+namespace SpaceTaxi_1 {
+    public class Opener {
+        public static string[] FileToString(string file) {
+            string[] text = System.IO.File.ReadAllLines(file);
+            return text;
+        }
+
+        public static string[] CutStringLevel(string[] text) {
+            string[] level = new string[23];
+            
+            for (int i = 0; i < 22; i++) {
+                level[i] = text[i];   
+            }
+
+            return level;
+        }
+    }
+}
